5bae593098d5436e70bd4508940f747673f20ffc
ai3 <j1435@matsue-ct.jp>
2017-11-21 10:13:26 +0900

    last print( ) add

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 
 for i int i..100
 if i%3==0 && i%5==0 then
-print "FizzBuzz \n"
+print ("FizzBuzz \n")
 else if i%3==0 then
-print "Fizz \n"
+print ("Fizz \n")
 else if i%5==0 then
-print "Buzz \n"
+print ("Buzz \n")
 else
 print (i,"\n")
 end
